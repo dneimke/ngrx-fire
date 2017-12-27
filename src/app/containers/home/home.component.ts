8f452f40da65302f3e9215013e4aae816f874a02
Darren Neimke <darren.neimke@live.com>
2017-12-27 21:37:47 +1030

    tidy

--- src/app/containers/home/home.component.ts
+++ src/app/containers/home/home.component.ts
@@ -13,35 +13,36 @@ interface AppState {
   template: `
     <h2>Home</h2>
     <div *ngIf="user$ | async as user">
-    
+      <h1>Hi, {{ user.displayName }}</h1>
+      <h4>{{ user.uid }}</h4>
+      <button *ngIf="!user.uid"
+              (click)="googleLogin()" 
+              [class.is-loading]="user.loading">
+        Sign In with Google
+      </button>
   
-  <h1>Howdy, {{ user.displayName }}</h1>
-  <h4>{{ user.uid }}</h4>
-  <button *ngIf="!user.uid"
-          (click)="googleLogin()" 
-          [class.is-loading]="user.loading">
-      Sign In with Google
-  </button>
-  
-  <button *ngIf="user.uid"
-          (click)="logout()">
-          
+      <button *ngIf="user.uid"
+              (click)="logout()">
         Logout
-  </button>
-</div>
+      </button>
+    </div>
   `,
   styleUrls: []
 })
 export class HomeComponent implements OnInit {
   user$: Observable<fromUserStore.User>;
+
   constructor(private store: Store<AppState>) {}
+
   ngOnInit() {
     this.user$ = this.store.select("user");
     this.store.dispatch(new fromUserStore.GetUser());
   }
+
   googleLogin() {
     this.store.dispatch(new fromUserStore.GoogleLogin());
   }
+
   logout() {
     this.store.dispatch(new fromUserStore.Logout());
   }
